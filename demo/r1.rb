9edd87bd6f69ee2c3f1d8f53a26634e59192e86f
Greenbirch2007 <291109028@qq.com>
2020-08-20 22:29:50 +0800

    8.20 chg

--- demo/r1.rb
+++ demo/r1.rb
@@ -1,6 +1,25 @@
 #!/usr/bin/ruby -w
 # -*- coding:utf-8 -*-
 #
-#
-#
-print <<
+# @name 是实例变量，能够被该类或子类继承引用
+
+class Player
+  def initialize(name="kaff")
+    @name = name
+
+
+  end
+
+
+  def show()
+    puts "player:#{@name}"
+  end
+end
+
+
+kona = Player.new()
+kona.show()
+
+curry = Player.new("curry")
+curry.show()
+
